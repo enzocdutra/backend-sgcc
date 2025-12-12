69dd542ca23c4812d7ed448c0be9104748d1f89d
Enzo Campão Dutra <128257870+enzocdutra@users.noreply.github.com>
2025-12-11 23:09:08 -0300

    server v0.05

--- src/server.js
+++ src/server.js
@@ -5,7 +5,13 @@ import authRoutes from "./routes/auth.routes.js";
 import clientRoutes from "./routes/client.routes.js";
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: 'https://frontend-sgcc.vercel.app', // Seu frontend
+  credentials: true, // Permite envio de credenciais
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/auth", authRoutes);
